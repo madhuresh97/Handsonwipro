aa1ac5ea4a75ffb020090292d24cbd23b1b46d5c
Madhuresh Gupta <madhuresh97@yahoo.co.in>
2019-04-20 17:54:52 +0530

    Area of Square

--- ForHandson/Program.cs
+++ ForHandson/Program.cs
@@ -6,7 +6,11 @@ namespace ForHandson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double side, area;
+            Console.WriteLine("Enter the side of square: ");
+            side = Convert.ToDouble(Console.ReadLine());
+            area = side * side;
+            Console.WriteLine("The area of square is: " + area);
         }
     }
 }
